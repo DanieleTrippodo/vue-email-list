138ae9e9b6e2b9b59d3fe94dc26f75562eda4762
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-05 16:47:59 +0200

    added methods for generate random email

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-  .then(response => {
-    console.log(response.data.response); // Stampa l'email ottenuta
-  });
+const app = Vue.createApp({
+    data() {
+      return {
+        emails: [] // Array per memorizzare le email generate
+      };
+    }
+});
+
+
+
+
+methods: {
+    // Metodo per generare una singola email
+    generateEmail() {
+
+        return axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                return response.data.response; // Ritorna l'email ottenuta
+            });
+    }
+}
