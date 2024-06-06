dd8117b9c2e2beffb4bcd2a21064f263eb3ec6ea
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-06 09:05:29 +0200

    fix the app.mount('#app')

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const app = Vue.createApp({
     data() {
-      return {
-        emails: [] // Array per memorizzare le email generate
-      };
-    }
-});
+        return {
+            emails: [] // Array per memorizzare le email generate
+        };
+    },
 
 
 
+    methods: {
+        // Metodo per generare una singola email
+        generateEmail() {
+            return axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => { return response.data.response; } );
+        },
 
-methods: {
-    // Metodo per generare una singola email
-    generateEmail() {
-        return axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => { return response.data.response; } );
-    }
-}
-
-
-// Metodo per generare 10 email e aggiungerle all'array emails
-  generateEmails() {
-    this.emails = []; // Resetta l'array emails
-    for (let i = 0; i < 10; i++) {
-      this.generateEmail().then(email => {
-        if (email) {
-          this.emails.push(email); // Aggiungi l'email all'array se non è null
+        generateEmails() {
+            this.emails = []; // Resetta l'array emails
+            for (let i = 0; i < 10; i++) {
+                this.generateEmail().then(email => {
+                    if (email) {
+                        this.emails.push(email); // Aggiungi l'email all'array se non è null
+                    }
+                });
+            }
         }
-      });
-    }
-  }
-}
+}});
+
+
+// nella versione precedente non funzionava perchè mi ero dimenticato di chiudere la instanza Vue...
+app.mount('#app');
